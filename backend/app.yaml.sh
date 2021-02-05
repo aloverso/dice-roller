de63f0eb7a7239fb855dbc4b731643c0a6893b92
Anne LoVerso <anne.loverso@gmail.com>
2021-02-05 13:08:34 -0500

    remove db references

--- backend/app.yaml.sh
+++ backend/app.yaml.sh
@@ -2,10 +2,5 @@
 echo """
 runtime: nodejs10
 env_variables:
-  DB_USER: postgres
-  DB_PASS: \"$DB_PASS\"
-  DB_NAME: \"$DB_NAME\"
-  DB_ENV: \"$DB_ENV\"
-  CLOUD_SQL_CONNECTION_NAME: \"$CLOUD_SQL_CONNECTION_NAME\"
   NODE_OPTIONS: --max_old_space_size=4096
 """
