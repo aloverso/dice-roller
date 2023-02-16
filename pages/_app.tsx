7afad51d9e555ae35a1a897c53fef0c28f0babdf
Anne LoVerso <anne.loverso@gmail.com>
2023-02-15 21:42:41 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,18 @@ import Head from "next/head";
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
   return (
     <div>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QPVERP6PDK"/>
+      <script
+        dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-QPVERP6PDK');
+          `,
+        }}
+      />
       <Head>
         <title>RPG Dice Roller</title>
       </Head>
